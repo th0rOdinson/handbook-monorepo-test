2ccb73e6c22e84a8e6d8b2eb25c68401b46c08d5
th0rOdinson <144699868+th0rOdinson@users.noreply.github.com>
2025-06-26 11:34:07 -0300

    base url going back agian to /

--- sites/optimism/docusaurus.config.ts
+++ sites/optimism/docusaurus.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
   url: "https://handbook-monorepo-test-wonderland.vercel.app",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/optimism/",
+  baseUrl: "/",
 
   presets: [
     [
